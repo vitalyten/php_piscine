2fe3a3f3b953e62f7a0f33d58a56259e4b4039c2
VitalyTen <vtenigin@student.42.us.org>
2017-03-02 19:04:43 -0800

    magnifying_glass.php

--- d02/ex02/magnifying_glass.php
+++ d02/ex02/magnifying_glass.php
@@ -9,8 +9,8 @@ function ft_toupper($match)
 if ($argc != 2)
 	return ;
 $str = file_get_contents($argv[1]);
-echo preg_replace_callback("/<a.*?\/a>/", function($match)
+echo preg_replace_callback("/<a.*?\/a>/si", function($match)
 	{
-		return (preg_replace_callback("/(?<=title=\").*?\"|>.*?</", 'ft_toupper', $match[0]));
+		return (preg_replace_callback("/(?<=title=\").*?\"|>.*?</si", 'ft_toupper', $match[0]));
 	}, $str);
 ?>
